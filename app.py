4f1e4011f0733b3c558d0e440a93027a7c8f3429
Akshatha MK <akshatha.mk.12.04@gmail.com>
2023-02-25 13:22:30 +0530

    Implementing feature points and landmarks into the person's body

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
+import os
+import cvzone
 import cv2
-#The above is to import the computer vision library 
-import mediapipe as mp
-#The above is to import the mediapipe library that is developed by Google
-#It has two main steps 
-#Hand_tracking and Palm_tracking
-import time
-#This is to check the frame rate 
- 
+from cvzone.PoseModule import PoseDetector
+
 cap = cv2.VideoCapture(0)
-#VideoCapture function is for Capturing the video from camera that is required.
-# 0 is the front camera of the Laptop 
-#Either ways
-'''
+detector = PoseDetector()
+
 while True:
-    success, img =cap.read()
-
-    cv2.imshow("Image",img)
+    success, img = cap.read()
+    img = cv2.resize(img, (1280, 720))
+    img = detector.findPose(img)
+    # img = cv2.flip(img,1)
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
-
-'''
-
-while cap.isOpened():
-    success, image = cap.read()
-
-    cv2.imshow("Image",image)
-    cv2.waitKey(1)
+                                         
