7af543f9410431892564fb05352ee7c466c390f5
Akshatha MK <akshatha.mk.12.04@gmail.com>
2023-02-25 20:49:36 +0530

    implementing button based routing in Flask

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask import Flask, render_template, Response
-import cv2
-from cvzone.PoseModule import PoseDetector
 import os
 import cvzone
-
-app = Flask(__name__)
+import cv2
+from cvzone.PoseModule import PoseDetector
 
 cap = cv2.VideoCapture(0)
 detector = PoseDetector()
 
 shirtFolderPath = "Resources/Shirts"
 listShirts = os.listdir(shirtFolderPath)
-fixedRatio = 262 / 190  # widthOfShirt/widthOfPoint11to12
-shirtRatioHeightWidth = 581 / 440
+# print(listShirts)
+fixedRatio = 300 / 190  # widthOfShirt/widthOfPoint11to12
+shirtRatioHeightWidth = 500 / 500
 imageNumber = 0
 imgButtonRight = cv2.imread("Resources/button.png", cv2.IMREAD_UNCHANGED)
 imgButtonLeft = cv2.flip(imgButtonRight, 1)
@@ -20,68 +18,52 @@ counterRight = 0
 counterLeft = 0
 selectionSpeed = 10
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+while True:
+    success, img = cap.read()
+    img = cv2.resize(img, (1280, 720))
+    img = detector.findPose(img)
+    # img = cv2.flip(img,1)
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
+    if lmList:
+        # center = bboxInfo["center"]
+        lm11 = lmList[11][1:3]
+        lm12 = lmList[12][1:3]
+        imgShirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
 
-def generate_frames():
-    counterRight = 0
-    counterLeft = 0
-    imageNumber=0
-    while True:
-        success, img = cap.read()
-        img = cv2.resize(img, (1280, 720))
-        if not success:
-            break
-        img = detector.findPose(img)
-        lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
-        if lmList:
-            lm11 = lmList[11][1:3]
-            lm12 = lmList[12][1:3]
-            imgShirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
+        widthOfShirt = int((lm11[0] - lm12[0]) * fixedRatio)
+        print(widthOfShirt)
+        imgShirt = cv2.resize(imgShirt, (widthOfShirt, int(widthOfShirt * shirtRatioHeightWidth)))
+        currentScale = (lm11[0] - lm12[0]) / 190
+        offset = int(48 * currentScale), int(48 * currentScale)
 
-            widthOfShirt = int((lm11[0] - lm12[0]) * fixedRatio)
-            imgShirt = cv2.resize(imgShirt, (widthOfShirt, int(widthOfShirt * shirtRatioHeightWidth)))
-            currentScale = (lm11[0] - lm12[0]) / 190
-            offset = int(44 * currentScale), int(48 * currentScale)
+        try:
+            img = cvzone.overlayPNG(img, imgShirt, (lm12[0] - offset[0], lm12[1] - offset[1]+10))
+        except:
+            pass
 
-            try:
-                img = cvzone.overlayPNG(img, imgShirt, (lm12[0] - offset[0], lm12[1] - offset[1]))
-            except:
-                pass
+        img = cvzone.overlayPNG(img, imgButtonRight, (1074, 293))
+        img = cvzone.overlayPNG(img, imgButtonLeft, (72, 293))
 
-            img = cvzone.overlayPNG(img, imgButtonRight, (1074, 293))
-            img = cvzone.overlayPNG(img, imgButtonLeft, (72, 293))
-
-            if lmList[16][1] < 300:
-                counterRight += 1
-                cv2.ellipse(img, (139, 360), (66, 66), 0, 0,
-                            counterRight * selectionSpeed, (0, 255, 0), 20)
-                if counterRight * selectionSpeed > 360:
-                    counterRight = 0
-                    if imageNumber < len(listShirts) - 1:
-                        imageNumber += 1
-            elif lmList[15][1] > 900:
-                counterLeft += 1
-                cv2.ellipse(img, (1138, 360), (66, 66), 0, 0,
-                            counterLeft * selectionSpeed, (0, 255, 0), 20)
-                if counterLeft * selectionSpeed > 360:
-                    counterLeft = 0
-                    if imageNumber > 0:
-                        imageNumber -= 1
-
-            else:
+        if lmList[16][1] < 300:
+            counterRight += 1
+            cv2.ellipse(img, (139, 360), (66, 66), 0, 0,
+                        counterRight * selectionSpeed, (0, 255, 0), 20)
+            if counterRight * selectionSpeed > 360:
                 counterRight = 0
+                if imageNumber < len(listShirts) - 1:
+                    imageNumber += 1
+        elif lmList[15][1] > 900:
+            counterLeft += 1
+            cv2.ellipse(img, (1138, 360), (66, 66), 0, 0,
+                        counterLeft * selectionSpeed, (0, 255, 0), 20)
+            if counterLeft * selectionSpeed > 360:
                 counterLeft = 0
+                if imageNumber > 0:
+                    imageNumber -= 1
 
-        ret, buffer = cv2.imencode('.jpg', img)
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            counterRight = 0
+            counterLeft = 0
 
-@app.route('/video')
-def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__=="__main__":
-    app.run(debug=True)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
